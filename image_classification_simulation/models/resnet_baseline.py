6729f25a962e13acede4e7fdd16b742a0b291225
SinaSarparast <26283630+SinaSarparast@users.noreply.github.com>
2022-06-23 16:37:35 -0400

    [skip ci] removed magic numbers

--- image_classification_simulation/models/resnet_baseline.py
+++ image_classification_simulation/models/resnet_baseline.py
@@ -31,12 +31,19 @@ class Resnet(BaseModel):
         self.feature_extractor = resnet18(
             pretrained=hyper_params["pretrained"]
         )
+        # freeze the feature extractor
+        if hyper_params["freeze_feature_extractor"]:
+            for param in self.feature_extractor.parameters():
+                param.requires_grad = False
 
         self.flatten = nn.Flatten()
-        self.linear1 = torch.nn.Linear(1000, hyper_params["size"])
+
+        dim_features = self.feature_extractor.fc.out_features
+        num_target_classes = hyper_params["num_classes"]
+        self.linear1 = torch.nn.Linear(dim_features, hyper_params["size"])
         self.linear2 = torch.nn.Linear(
-            hyper_params["size"], hyper_params["num_classes"]
-        )  # 964: number of classes in Omniglot
+            hyper_params["size"], num_target_classes
+        )
         self.activation = torch.nn.ReLU()
 
     def _generic_step(self, batch: typing.Any, batch_idx: int) -> typing.Any:
@@ -78,7 +85,7 @@ class Resnet(BaseModel):
         """
         probs = nn.functional.softmax(logits, 0)
         preds = torch.argmax(probs, 1)
-        return (preds == targets).sum().item() / len(targets)
+        return (preds == targets).sum() / len(targets)
 
     def training_step(
         self, batch: torch.Tensor, batch_idx: torch.Tensor
@@ -98,10 +105,23 @@ class Resnet(BaseModel):
             loss produced by the loss function.
         """
         loss, logits = self._generic_step(batch, batch_idx)
+        input_data, targets = batch
+        train_acc = self.compute_accuracy(logits, targets)
         self.log("train_loss", loss)
+        self.log("train_acc", train_acc)
         self.log("epoch", self.current_epoch)
         self.log("step", self.global_step)
-        return loss
+        return {"loss": loss, "acc": train_acc}
+
+    def training_epoch_end(self, training_step_outputs):
+        overal_train_acc = torch.stack(
+            [i["acc"] for i in training_step_outputs]
+        ).mean()
+        overal_train_loss = torch.stack(
+            [i["loss"] for i in training_step_outputs]
+        ).mean()
+        self.log("overall_train_loss", overal_train_loss)
+        self.log("overall_train_acc", overal_train_acc.item())
 
     def validation_step(
         self, batch: torch.Tensor, batch_idx: torch.Tensor
@@ -124,7 +144,15 @@ class Resnet(BaseModel):
         input_data, targets = batch
         val_acc = self.compute_accuracy(logits, targets)
         self.log("val_loss", loss)
-        self.log("val_acc", val_acc)
+        self.log("val_acc", val_acc.item())
+        return val_acc
+
+    def validation_epoch_end(self, validation_step_outputs):
+        # last batch is always smaller than the others
+        # we are not accounting for it here
+        overall_val_acc = torch.stack(validation_step_outputs).mean()
+        self.log("overall_val_acc", overall_val_acc)
+        return overall_val_acc
 
     def test_step(
         self, batch: torch.Tensor, batch_idx: torch.Tensor
