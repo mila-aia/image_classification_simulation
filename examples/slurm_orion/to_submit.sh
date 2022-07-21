06780bfc6edf80d0dbeca2a074ba80926fc0f201
alzaia <aldo.zaimi@mila.quebec>
2022-07-21 16:25:11 -0400

    orion updates

--- examples/slurm_orion/to_submit.sh
+++ examples/slurm_orion/to_submit.sh
@@ -15,8 +15,8 @@ export MLFLOW_TRACKING_URI='mlruns'
 export ORION_DB_ADDRESS='orion_db.pkl'
 export ORION_DB_TYPE='pickleddb'
 
-orion -v hunt --config orion_config.yaml \
-    main --data ../data --config config.yaml --disable-progressbar \
+orion -v hunt --config /home/mila/a/aldo.zaimi/demos_code/image_classification_simulation/examples/slurm_orion/orion_config.yaml \
+    main --data /home/mila/a/aldo.zaimi/data/domain_adaptation_images/amazon/images --config /home/mila/a/aldo.zaimi/demos_code/image_classification_simulation/examples/conv_ae/config.yaml --disable-progressbar \
     --output '{exp.working_dir}/{exp.name}_{trial.id}/' \
     --log '{exp.working_dir}/{exp.name}_{trial.id}/exp.log' \
     --tmp-folder ${SLURM_TMPDIR}
