2c51faee0797c88150864de7c168bbf42c9a451f
alzaia <aldo.zaimi@mila.quebec>
2022-07-22 11:53:54 -0400

    updating config files for orion

--- examples/slurm_orion/to_submit.sh
+++ examples/slurm_orion/to_submit.sh
@@ -4,7 +4,7 @@
 #SBATCH --cpus-per-task=2
 #SBATCH --gres=gpu:1
 #SBATCH --mem=5G
-#SBATCH --time=0:05:00
+#SBATCH --time=4:00:00
 #SBATCH --output=logs/%x__%j.out
 #SBATCH --error=logs/%x__%j.err
 # remove one # if you prefer receiving emails
@@ -15,8 +15,6 @@ export MLFLOW_TRACKING_URI='mlruns'
 export ORION_DB_ADDRESS='orion_db.pkl'
 export ORION_DB_TYPE='pickleddb'
 
-orion -v hunt --config /home/mila/a/aldo.zaimi/demos_code/image_classification_simulation/examples/slurm_orion/orion_config.yaml \
-    main --data /home/mila/a/aldo.zaimi/data/domain_adaptation_images/amazon/images --config /home/mila/a/aldo.zaimi/demos_code/image_classification_simulation/examples/conv_ae/config.yaml --disable-progressbar \
-    --output '{exp.working_dir}/{exp.name}_{trial.id}/' \
-    --log '{exp.working_dir}/{exp.name}_{trial.id}/exp.log' \
-    --tmp-folder ${SLURM_TMPDIR}
+orion -v hunt --config orion_config.yaml \
+    main --data $HOME/data/domain_adaptation_images/amazon/images --config config.yaml --start-from-scratch --gpus 0 \
+    --output '{exp.working_dir}/{exp.name}_{trial.id}/' --log '{exp.working_dir}/{exp.name}_{trial.id}/exp.log' --tmp-folder ${SLURM_TMPDIR}
